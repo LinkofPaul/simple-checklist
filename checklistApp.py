3fe3f3580f27e4ed18486b3dab886fed6222ecdd
Paul Kapust <paul.kapust@tuhh.de>
2019-10-03 22:02:46 +0200

    First steps with java script/ creation field

--- checklistApp.py
+++ checklistApp.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
@@ -6,6 +6,11 @@ app = Flask(__name__)
 def index():
     return render_template("index.html", title="Checklist")
 
+# @app.route("/setup", methods=["POST"])
+# def setup():
+#     check here if name is already taken
+#     save credentails 
+#     and redirect to site which allows editing of checklist
 
 if __name__ == '__main__':
     app.run()
