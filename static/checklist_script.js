1a3170e3b44eecfb1c3d168dc199dfe02f97c68c
Paul Kapust <paul.kapust@tuhh.de>
2020-02-18 14:37:30 +0100

    Short update

--- static/checklist_script.js
+++ static/checklist_script.js
@@ -1,11 +1,9 @@
 $('.task_remove_button').hide();
 
 function show_remove_button(task_name) {
-    console.log(task_name)
     $("#" + task_name).show();
 };
 
 function hide_remove_button(task_name) {
-    console.log(task_name)
     $("#" + task_name).hide();
 };
