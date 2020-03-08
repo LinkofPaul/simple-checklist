c5835fdc7ea4e56d95d0e4f0efddb82e0590c384
Paul Kapust <paul.kapust@tuhh.de>
2020-03-08 20:27:33 +0100

    Allowing resizing and changing orientation

--- static/checklist_script.js
+++ static/checklist_script.js
@@ -1,13 +1,5 @@
 $('.task_remove_button').hide();
 
-function show_remove_button(task_name) {
-    $("#" + task_name).show();
-};
-
-function hide_remove_button(task_name) {
-    $("#" + task_name).hide();
-};
-
 function toggle_remove_button(task_name) {
     $("#" + task_name).toggle();
 };
